d750595a0e886942686e8297a87abe7fafe508b0
Diego Oliveira <diego@pagimax.cloud>
2024-10-21 14:05:34 -0300

    Update go.mod and go.sum with new dependencies

---
+++ api/src/config/config.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+var (
+	// StringConexaoBanco é a string de conexão com o MySQL
+	StringConexaoBanco = ""
+	// Porta onde a API vai estar rodando
+	Porta = 0
+)
+
+// Carregar vai inicializar as variáveis de ambiente
+func Carregar() {
+	var err error
+
+	if err = godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+
+	Porta, err = strconv.Atoi(os.Getenv("API_PORT"))
+	if err != nil {
+		Porta = 9000
+	}
+
+	StringConexaoBanco = os.Getenv("CONNSTR")
+}
